8742c6e5ad0cef8f37b12f02deb7ab49f6500318
冬铭 <minghao.rmh@alibaba-inc.com>
2017-03-08 19:15:18 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Parse = undefined;
 
-var _lodash = require('lodash');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-var _formatInstruction = require('./mod/formatInstruction');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _instruction = require('./mod/instruction');
 
 var Parse = exports.Parse = function Parse(string, data, callback) {
 
 	string = string.toString();
 
-	var instruction = (0, _formatInstruction.getInstruction)(string);
-	var compiled = (0, _formatInstruction.formatInstruction)(instruction, data);
+	var instruction = (0, _instruction.getInstruction)(string);
+	var compiled = (0, _instruction.formatInstruction)(instruction, data);
 
 	callback && callback(compiled);
 };
